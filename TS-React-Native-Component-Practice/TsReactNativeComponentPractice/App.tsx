3fa365774db54214a0b251ec5fd33167093f78d0
ekelly55 <eamonn.michael.kellyr@email.com>
2023-06-06 16:42:38 -0400

    App.tsx rewritten as valid TS

--- TS-React-Native-Component-Practice/TsReactNativeComponentPractice/App.tsx
+++ TS-React-Native-Component-Practice/TsReactNativeComponentPractice/App.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { FC } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
-const App = () => {
+interface AppProps {
+  // Define props for your component here
+}
+
+const App: FC<AppProps> = ({ /* Destructure props here */ }) => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Hello there! Welcome to TypeScript in React Native!</Text>
+    <View style={styles.container}>
+      <Text>Hello there! This is React Native with Typescript</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default App;
